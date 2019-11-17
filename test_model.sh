8320d69f325eb2b36dbf6bcdfaa72cb5084c76f7
Ilya <ilya.vouk@gmail.com>
2019-11-17 20:15:24 +0300

    Add script to test a model

---
+++ test_model.sh
@@ -0,0 +1,20 @@
+# Settings
+MODEL=vgg16
+DATASET=pascal_voc
+
+
+# Session specification
+SESSION=1
+EPOCH=1
+CHECKPOINT=10021
+
+# Paths
+OUTPUT_FOLDER="models/"$MODEL"/"$DATASET"/faster_rcnn_"$EPOCHS"ep"
+STDOUT_PATH=$OUTPUT_FOLDER"/test_stdout.txt"
+STDERR_PATH=$OUTPUT_FOLDER"/test_stderr.txt"
+
+
+# Run main script
+python test_net.py --dataset $DATASET --net $MODEL \
+                   --checksession $SESSION --checkepoch $EPOCH --checkpoint $CHECKPOINT \
+                   --cuda 1> $STDOUT_PATH 2> $STDERR_PATH
